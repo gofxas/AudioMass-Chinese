0e12b2f67bd79589a2a4e7c1ecb1972ea66c3bb9
刘传 <769601722@qq.com>
2023-03-28 15:04:47 +0800

    git commit -m "first commit"

---
+++ welcome.js
@@ -0,0 +1,71 @@
+(function ( w, d, PKAE ) {
+'use strict';
+
+setTimeout(function () {
+
+	PKAudioEditor._deps.Wlc = function () {
+			var body_str = '';
+			var body_str2 = '';
+
+			if (PKAE.isMobile) {
+				change -= 15;
+				body_str = '提示:<br/>请确保您的设备不处于静音模式。 您可能需要实际翻转无声开关。 '+
+				'<img src="phone-switch.jpg" style="max-width:224px;max-height:126px;width:40%;margin: 10px auto; display: block;"/>'+
+				'<br/><br/>';
+			}
+			else {
+				body_str = '提示:<br/>请记住，大多数钥匙快捷方式都依赖于<strong> shift + <u>键</u> </strong>组合。 （例如，移动+z用于撤消，Shift+C复制，Shift+X Cut ...等）<br/><br/>';
+				body_str2 = '查看代码库 <a href="https://github.com/pkalogiros/audiomass" target="_blank">Github</a><br/><br/>'; // checkout the code on github
+			}
+
+			// Welcome to AudioMass,
+			var md = new PKSimpleModal({
+				title: '<font style="font-size:15px">欢迎使用 AudioMass</font>',
+				ondestroy: function( q ) {
+					PKAE.ui.InteractionHandler.on = false;
+					PKAE.ui.KeyHandler.removeCallback ('modalTemp');
+			},
+			body:'<div style="overflow:auto;-webkit-overflow-scrolling:touch;max-width:580px;width:calc(100vw - 40px);max-height:calc(100vh - 340px);min-height:110px;font-size:13px; color:#95c6c6;padding-top:7px;">'+
+				'Audiomass是一个免费的开源，基于Web的音频和波形编辑器。<br />它完全在没有后端的浏览器中运行，无需插件!'+
+				'<br/><br/><br/>'+
+				body_str+
+				'您可以加载任何类型的音频您的浏览器支持和执行操作，例如淡出，切割，修剪，更改音量，'+
+				'并应用大量音频效果。<br/><br/>'+
+				body_str2+
+				'</div>',
+			setup:function( q ) {
+					PKAE.ui.InteractionHandler.checkAndSet ('modal');
+					PKAE.ui.KeyHandler.addCallback ('modalTemp', function ( e ) {
+						q.Destroy ();
+					}, [27]);
+
+					// ------
+					var scroll = q.el_body.getElementsByTagName('div')[0];
+					scroll.addEventListener ('touchstart', function(e){
+						e.stopPropagation ();
+					}, false);
+					scroll.addEventListener ('touchmove', function(e){
+						e.stopPropagation ();
+					}, false);
+
+					// ------
+				}
+			});
+			md.Show ();
+			document.getElementsByClassName('pk_modal_cancel')[0].innerHTML = '&nbsp; &nbsp; &nbsp; 好的 &nbsp; &nbsp; &nbsp;';
+	};
+
+	var change = 96;
+	var exists = w.localStorage && w.localStorage.getItem ('k');
+
+	if (!exists) {
+		change = 0;
+		w.localStorage && w.localStorage.setItem ('k', 1);
+	}
+
+	if ( ((Math.random () * 100) >> 0) < change) return ;
+	PKAudioEditor._deps.Wlc ();
+
+}, 320);
+
+})( window, document, PKAudioEditor );
